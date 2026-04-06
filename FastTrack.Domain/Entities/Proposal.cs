82d7f57053837356621e8397c13b008737a1171a
caledcresst-dev <caled.cresst@gmail.com>
2026-04-06 16:54:29 -0300

    Criação da entidade Proposal e enum com validações

---
+++ FastTrack.Domain/Entities/Proposal.cs
@@ -0,0 +1,33 @@
+using FastTrack.Domain.Enums;
+
+namespace FastTrack.Domain.Entities;
+
+public class Proposal
+{
+    public Guid Id { get; private set; }
+    public string Document { get; private set; }
+    public decimal RequestedAmount { get; private set; }
+    public int Installments { get; private set; }
+    public ProposalStatus Status { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public Proposal(string document, decimal amount, int installments)
+    {
+        if (string.IsNullOrWhiteSpace(document) || document.Length != 11)
+            throw new ArgumentException("CPF deve ter 11 dígitos");
+        if (amount < 100 || amount > 100000)
+            throw new ArgumentException("O valor soliciado deve estar entre R$ 100 e R$100.000");
+            if(installments < 1 || installments > 48)
+            throw new ArgumentException("O parcelamento deve ser entre 1 e 48 meses.");
+
+            Id = Guid.NewGuid();
+            Document = document;
+            RequestedAmount = amount;
+            Installments = installments;
+            Status = ProposalStatus.Pending;
+            CreatedAt = DateTime.UtcNow;
+    }
+public void Approve() => Status = ProposalStatus.Approved;
+    public void Reject() => Status = ProposalStatus.Rejected;
+
+}
---
+++ FastTrack.Domain/Enums/ProposalStatus.cs
@@ -0,0 +1,9 @@
+namespace FastTrack.Domain.Enums;
+
+public enum ProposalStatus
+{
+    Pending = 1,
+    Approved = 2,
+    Rejected = 3,
+    UnderAnalysis = 4
+}
